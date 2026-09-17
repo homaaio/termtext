c00f1d176d2d88520f5fa1c22f890625a09bd037
homaaio <homa4ella@gmail.com>
2026-09-17 22:07:37 +0500

    added enter scroll

--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <termios.h>
+#include <unistd.h>
 #define MAX_LINES 1000
-
-// "tt <file_name>" command:
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -18,31 +19,34 @@ int main(int argc, char *argv[]) {
         perror("tt");
         return 1;
     }
-    char *lines[1000];
+    char *lines[MAX_LINES];
     char buf[1024];
     int count = 0;
     int offset = 0;
     int screen_height = 20;
 
+    struct termios orig;
+    tcgetattr(STDIN_FILENO, &orig);
+    struct termios raw = orig;
+    cfmakeraw(&raw);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
     while (fgets(buf, sizeof(buf), file)) {
+        buf[strcspn(buf, "\n")] = 0;
         lines[count] = strdup(buf);
         count++;
-        // count = count + 1;
-        // printf("%s", buf);
     }
+
     while (offset < count) {
+        printf("\033[2J\033[H");
         for (int i = offset; i < offset + screen_height && i < count; i++) {
-        printf("%d %s", i, lines[i]);
+            printf("%d %s\r\n", i, lines[i]);
+        }
+        getchar();
+        offset += screen_height;
     }
-    getchar();
 
-    offset += screen_height;
-    }
-    // not read = not open
-    for (int i = offset; i<offset + screen_height && i<count; i++) {
-        printf("%d %s", i ,lines[i]);
-    }
     fclose(file);
-
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig);
     return 0;
 }
